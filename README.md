daf4bf23eaaaca00360859f8506e49cdfa88891e
taytayfujif <31020217+taytayfujif@users.noreply.github.com>
2017-10-20 14:16:28 -1000

    first commit

--- README.md
+++ README.md
@@ -1,50 +1,15 @@
-# Welcome To The Pet Adoption Store!
-You will be creating a web app that helps pets find homes!
+# js-functions
 
-## Before you begin
-Please create a mockup of your website for an instructor to approve :)
+##Getting Started
+1. **Fork** this repository
+2. **Clone** this repository from your personal Github account:
+    - Copy the HTTPS address on the page.
+    - From you 'teachertraining' folder, run the command `$ git clone [HTTP address]` in your terminal in order to clone this repository into that folder.
+3. From your terminal, navigate into the js-functions assignment:
+    - `$ cd js-functions`
+4. Open the js-functions assignment in sublime and write your code in the `exercises.js` file.
+5. From your terminal, use __node__ to test your code:
+   - `node exercises.js` (don't forget to console.log your work)
+   
 
-## Set-up your files
-Please create the following files:
-- index.html
-- styles.css
-- README.md
-
-Create a link tag for your CSS stylesheet in your html document.
-
-## HTML
-1. Create a header containing the name of your pet store using a **header** tag and set the value of the id attribute to `store-name`.
-
-2. Create a container for all of your pet profiles using a **div** tag and set the value of the id attribute to `all-pets`.
-
-3. Create at least 5 individual pet profiles using a **div** tag and set the value of the class attribute to `individual-pet` for each profile.
-
-4. Find five images of your five different pets on the internet.
-
-5. For each pet profile:
-  - Create a Header containing the pet's name using a **header** tag smaller than **h1**.
-  - Create a profile photo for each pet using an **image** tag and set the value of the source attribute to the image's URL.
-  - Create a description for each pet using a **paragraph** tag.
-
-*Stretch Goals* Wouldn't it be cool if each profile could make a sound? Add an audio tag to each pet profile.
-
-## CSS
-1. Set the background color of the body using the `element` selector.
-
-2. Change the font color of the header using the `id` selector.
-
-3. Change the styles of the pet profiles using the `class` selector.
-  - Align all the pet profiles and their content to the center of their container element.
-  - Set all pet profile photos to the same size.
-  - Align all pet profile text to the center.
-
-4. Modify your elements with these style attributes:
-  - `background-color`
-  - `color`
-  - `font-size`
-  - `font-family`
-  - `height`
-  - `text-align`
-  - `width`
-
-*Stretch Goals* Use at least 5 different style attributes that we did not mention!
+    
---
+++ exercises.js
@@ -0,0 +1,299 @@
+/*
+* A function is a block of code (a series of instructions) designed to execute a certain task. Functions allow you to reuse code.
+ 
+* i.e. A function to generate a greeting to specific person you designate:*/
+
+// Step One: Define the function.
+function greeting(guest){ // takes in a parameter named 'guest' which acts like a local variable inside the function
+ return "Good afternoon " + guest + ".";  // the output value after the function is invoked
+}
+greeting("Mr. Smith"); // invoking or calling the function to execute the code (instructions) inside the function.
+
+// Step Two: Assign the function invocation to a variable.
+var testGreeting = greeting("Mr. Smith"); // assigning the function call into a variable 
+
+// Step Three: Console.log the variable to test function output!
+console.log(testGreeting); // printing the output value of the function.
+
+// From your terminal, try running this file with node in order to see the result of `console.log(testGreeting);`
+
+/*Below are some specifications for Functions to be built. */
+/*
+ * #1
+ * Declare two variables
+ *   @variable Datatype: Number `bango1`
+ *   @variable Datatype: Number `bango2`
+ *
+ * These two variables will be used to invoke the functions #2 - #5.
+*/ 
+var= bango1
+var= bango2
+/*
+ * #2
+ * Function - add
+ * Create a function named `add` which will take two parameters: `num1` and `num2`.
+ * 
+ *    @param Datatype: Number `num1`
+ *    @param Datatype: Number `num2`
+ *    @return Datatype: Number
+ *
+ * This function will add two numbers (the parameters, `num1` and `num2`) and 
+ * return the sum of these two numbers.
+ * Invoke the function and assign it to a variable named `sum`.
+ * Console.log `sum` to test your code.
+*/
+Function add (num1; num2){
+ return "sum"; 
+}
+add (4,3);
+var sum = add (4,3);
+console.log("sum");
+
+
+
+/*
+ * #3
+ * Function - subtract
+ * Create a function named `subtract` which will take two parameters: `num1` and `num2`.
+ *
+ *    @param Datatype: Number `num1`
+ *    @param Datatype: Number `num2`
+ *    @return Datatype: Number
+ *
+ * This function will subtract two numbers and return the difference of these two numbers.
+ * Invoke the function and assign it to a variable named `difference`.
+ * Console.log `difference` to test your code.
+*/ 
+function subtract (num1;num2){
+	return "number";
+}
+subtract(4,3);
+var difference = subtract(4,3);
+console.log("difference")
+
+
+/*
+ * #4
+ * Function - multiply
+ * Create a function named `multiply` which will take two parameters: `num1` and `num2`.
+ *
+ *    @param Datatype: Number `num1`
+ *    @param Datatype: Number `num2`
+ *    @return Datatype: Number
+ *
+ * This function will multiply two numbers and return the product of these two numbers.
+ * Invoke the function and assign it to a variable named `product`.
+ * Console.log `product` to test your code.
+*/
+function multiply  (num1;num2){
+	return "number";
+}
+multiply(4,3);
+var product = multiply(4,3);
+console.log("product")
+
+/*
+ * #5
+ * Function - divide
+ * Create a function named `divide` which will take two parameters: `num1` and `num2`.
+ *
+ *    @param Datatype: Number `num1`
+ *    @param Datatype: Number `num2`
+ *    @return Datatype: Number
+ *
+ * This function will divide two numbers and return the quotient of these two numbers.
+ * Invoke the function and assign it to a variable named `quotient`.
+ * Console.log `quotient` to test your code.
+*/
+function divide (num1;num2){
+	return "number";
+}
+divide(4,3);
+var quotient= divide(4,3);
+console.log("quotient")
+
+/*
+ * #6
+ * Function - checkSum
+ * Create a function named `checkSum` which will take a parameter: `x`.
+ *
+ *   @param Datatype: Number `x`
+ *   @return Datatype: String
+ *
+ * This function will return the string "Mariah Carey has been married `x` amount of times." Where `x` is the value when you invoke the function. Invoke this function using the variable `sum`. 
+ * Console.log your result.
+*/
+function checksum(x){
+	return "string";
+}
+console.log()
+
+/*
+ * #7
+ * Function - checkDifference
+ * Create a function named `checkDifference` which will take a parameter `x`.
+ *
+ *    @param Datatype: Number `x`
+ *    @return Datatype: String
+ *
+ * This function will return the string "Last night I dreamt that I ate `x` Taco Bell soft tacos."  Where `x` is the value when you invoke the function. Invoke this function using the variable `difference`.
+ * Console.log your results.
+*/ 
+
+
+
+/*
+ * #8
+ * Function - checkProduct
+ * Create a function named `checkProduct` which does not require any parameters.
+ *
+ *    @return Datatype: Number
+ *
+ * This function will multiply the values stored in the sum and product variables.
+ * Console.log your result.  
+*/
+
+
+
+/*
+ * #9
+ * Function - checkQuotient
+ * Create a function named `checkQuotient` which does not require any parameters.
+ *
+ *    @return Datatype: Number
+ *
+ * This function will multiply the values stored in the product and quotient variables.
+ * Console.log your result.  
+*/
+
+
+
+/*
+ * #10
+ * Function - addThenSubtract
+ * Declare three variables
+ *
+ *   @variable Datatype: Number `bango3`
+ *   @variable Datatype: Number `bango4`
+ *   @variable Datatype: Number `bango5`
+ *   
+ * Next, create a function named addThenSubtract which takes three parameters: `num1`, `num2`, `num3`.
+ *
+ *    @param Datatype: Number `num1`
+ *    @param Datatype: Number `num2`
+ *    @param Datatype: Number `num3`
+ *    @return Datatype: Number
+ *
+ * This function will add the first two parameters together. Then with the sum of that operation, subtract it from the third parameter. PLEASE USE YOUR PREVIOUS FUNCTIONS FOR THIS EXERCISE. 
+ * Console.log your result.
+*/ 
+
+
+
+/*
+ * #11
+ * Function - multiplyThenDivide
+ * Create a function named multiplyThenDivide which takes three parameters: `num1`, `num2`, `num3`.
+ *
+ *    @param Datatype: Number `num1`
+ *    @param Datatype: Number `num2`
+ *    @param Datatype: Number `num3`
+ *    @return Datatype: Number
+ *
+ * This function will multiply the first two parameters together. Then with the product of that operation, divide it from the third parameter. PLEASE USE YOUR PREVIOUS FUNCTIONS FOR THIS EXERCISE. 
+ * Console.log your result.
+*/ 
+
+
+
+/*
+ * #12
+ * Function - createFullName
+ * Create a function named createFullName which takes two parameters: `firstName`, `lastName`.
+ *
+ *   @param Datatype: String `firstName`
+ *   @param Datatype: String `lastName`
+ *   @return Datatype: String
+ *
+ *  This function `returns` back a string which represents someone's full name. Invoke this function by passing in your first  and last name into the function. Store the return value into a variable named `myFullName` and console.log this variable to show your result.
+*/
+
+
+
+/*
+ * #13 
+ * Function - eatFood
+ * Create a function named eatFood which takes three parameters: `firstName`, `lastName`, `food`.
+ *
+ *   @param Datatype: String `firstName`
+ *   @param Datatype: String `lastName`
+ *   @param Datatype: String `food`
+ *   @return Datatype: String
+ *
+ * This function will use the previous `createFullName` function above to create a message. For example, if you invoked the function like this: eatFood("Biggie", "Smalls", "Won Ton Soup"); it will display the message "Biggie Smalls eats Won Ton Soup everyday for breakfast."
+ * Console.log your result.
+ */
+
+
+
+/************** ENGAGE HYPERDRIVE **************/
+/* No more training wheels! For the exercises #14-18, use the experience you've
+*  gained figure out how to build the necessary functions. Use your google-fu to 
+*  figure out anything you don't know! Ganbatte (good luck)! */
+/************** FIRE ALL CANNONS ***************/
+
+/*
+ * #14
+ * Function shoeSize
+ * Create a function that will take in a parameter named `inches`. This function will convert inches to centimeters(cm).
+ * Console.log your result. 
+*/
+
+
+
+/*
+ * #15
+ * Function allCaps
+ * Create a function that will take in a parameter named `str`. 
+ * This function will capitalize all the letters in the string. 
+ * Console.log your result.
+ * Example input: "believe you can and you're halfway there."
+ * Example output: "BELIEVE YOU CAN AND YOU'RE HALFWAY THERE."
+*/
+ 
+
+
+/*
+ * #16
+ * Function oneCap
+ * Create a function that will take in a parameter named `str`. 
+ * This function will capitalize only the first letter in the variable phase above. 
+ * Console.log your result.
+*/
+
+
+
+/*
+ * #17
+ * Function - verifyDrinkingAge
+ * Create a function named verifyDrinkingAge which takes a parameter named `age`. 
+ * This function returns a Boolean value, `true` or `false`, depending on 
+ * whether `age` is above or below the legal drinking age in the state of Hawaii.
+ *
+ * Call this function and pass in a number value.
+ * Store the return value to a variable named `canDrink`. Console.log your result.
+ */
+
+
+
+/**
+ * #18
+ * Function - throwParty
+ * Create a function named throwParty. This function will check the value stored in the `canDrink` variable in the previous exercise. If the value is true, it will return the message "Cheee Hoo! We going to da party!" If false, then it will return the message "Meh, see you at Starbucks." Store the return value to a variable named `canParty`. Console.log your result.
+ */
+
+
+
+
+
+
